8bceb51ea85cb743b9b8fb8541f29c17108d9a49
parker7johnson <ptj7@uw.edu>
2024-07-02 22:29:52 -0700

    writing to request

--- app/server.go
+++ app/server.go
@@ -19,9 +19,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
+	defer conn.Close()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	status := []byte("HTTP/1.1 200 OK\r\n\r\n")
+	conn.Write(status)
 }
